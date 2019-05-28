cf3bf644e2d6405d540fdcabf7faec308604f7ba
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-28 13:52:17 +0000

    Deploy justusjonas74/vag-departures to github.com/justusjonas74/vag-departures.git:gh-pages

---
+++ precache-manifest.56a17c193aa4d3256ee1680440bddf9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d097488b2d0382d060f54f93c0a7e9",
+    "url": "/vag-departures/index.html"
+  },
+  {
+    "revision": "a2f2f44857ec3a148f0d",
+    "url": "/vag-departures/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "27170dff251d4673ccc2",
+    "url": "/vag-departures/static/css/main.7c006215.chunk.css"
+  },
+  {
+    "revision": "a2f2f44857ec3a148f0d",
+    "url": "/vag-departures/static/js/2.7f4f3bf7.chunk.js"
+  },
+  {
+    "revision": "27170dff251d4673ccc2",
+    "url": "/vag-departures/static/js/main.0d4d56c0.chunk.js"
+  },
+  {
+    "revision": "0cab3a78800e0164e96e",
+    "url": "/vag-departures/static/js/runtime~main.34e43f60.js"
+  },
+  {
+    "revision": "86377fca9a2188d4d1eaafa1576cf3e1",
+    "url": "/vag-departures/static/media/doodles.86377fca.png"
+  }
+]);
